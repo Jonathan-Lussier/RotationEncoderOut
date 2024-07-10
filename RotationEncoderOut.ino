dec53471a76787e6e77ea84b7a56e74f442b2676
Jonathan-Lussier <88896792+Jonathan-Lussier@users.noreply.github.com>
2024-07-10 14:49:36 -0600

    updated for 13pin deployment verison

--- RotationEncoderOut.ino
+++ RotationEncoderOut.ino
@@ -6,22 +6,6 @@ Encoder nemaPos(2, 3);
 long oldPosition = -999;
 unsigned long currentPos;
 unsigned long timeNow;
-bool bit1;
-bool bit2;
-bool bit3;
-bool bit4;
-bool bit5;
-bool bit6;
-bool bit7;
-bool bit8;
-bool bit9;
-bool bit10;
-bool bit11;
-bool bit12;
-bool bit13;
-bool bit14;
-bool bit15;
-bool bit16;
 int bPin1 = 4;
 int bPin2 = 5;
 int bPin3 = 6;
@@ -42,8 +26,6 @@ int bPin16 = 19;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("code start.");
   nemaPos.write(4000);
 
   pinMode(bPin1, OUTPUT);
@@ -95,29 +77,6 @@ void loop() {
 
   if (currentPos != oldPosition){
     oldPosition = currentPos;
-    Serial.println(currentPos);
-    Serial.println(timeNow);
-
-    
-     bit1 = currentPos & 0b1;
-     bit2 = currentPos & 0b10;
-     bit3 = currentPos & 0b100;
-     bit4 = currentPos & 0b1000;
-     bit5 = currentPos & 0b10000;
-     bit6 = currentPos & 0b100000;
-     bit7 = currentPos & 0b1000000;
-     bit8 = currentPos & 0b10000000;
-     bit9 = currentPos & 0b100000000;
-     bit10 = currentPos & 0b1000000000;
-     bit11 = currentPos & 0b10000000000;
-     bit12 = currentPos & 0b100000000000;
-     bit13 = currentPos & 0b1000000000000;
-     bit14 = currentPos & 0b10000000000000;
-     bit15 = currentPos & 0b100000000000000;
-     bit16 = currentPos & 0b100000000000000;
-    //Serial.println(bit1+bit2+bit3+bit4+bit5+bit6+bit7+bit8+bit9+bit10+bit11+bit12+bit13+bit14+bit15+bit16);
-   // Serial.println(bit1+bit2+bit3+bit4+bit5+bit6+bit7+bit8);
-    
 
     digitalWrite(bPin1, currentPos & 0b1);
     digitalWrite(bPin2, currentPos & 0b10);
@@ -135,36 +94,10 @@ void loop() {
     digitalWrite(bPin14, currentPos & 0b10000000000000);
     digitalWrite(bPin15, currentPos & 0b100000000000000);
     digitalWrite(bPin16, currentPos & 0b1000000000000000);
-
-    
-    Serial.println(" ");
-
-    Serial.print(bit16);
-    Serial.print(bit15);
-    Serial.print(bit14);
-    Serial.print(bit13);
-    Serial.print(bit12);
-    Serial.print(bit11);
-    Serial.print(bit10);
-    Serial.print(bit9);
-    Serial.print(bit8);
-    Serial.print(bit7);
-    Serial.print(bit6);
-    Serial.print(bit5);
-    Serial.print(bit4);
-    Serial.print(bit3);
-    Serial.print(bit2);
-    Serial.print(bit1);
-
-    Serial.println(" ");
-    
-
   }
   if (currentPos <= 0 || currentPos >= 8000){
     nemaPos.write(4000);
   }
-  delay(9);
-
-
+  delay(5);
 }
 
